bafc2089df56efcd4631e61784580be99bb969e4
C4ptnS4lty <jonlahmann@gmail.com>
2024-11-24 16:00:30 -0500

    finish basics

--- binary-tree.js
+++ binary-tree.js
@@ -16,29 +16,88 @@ class BinaryTree {
   /** minDepth(): return the minimum depth of the tree -- that is,
    * the length of the shortest path from the root to a leaf. */
 
-  minDepth() {
+  minDepth(branch = this.root) {
+    if (!branch) return 0;
 
+    let leftStep = 1;
+    let rightStep = 1;
+
+    if (branch.left) {
+      leftStep += this.minDepth(branch.left);
+    }
+
+    if (branch.right) {
+      rightStep += this.minDepth(branch.right);
+    }
+
+    if (leftStep > rightStep) return rightStep;
+    return leftStep;
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
-  maxDepth() {
+  maxDepth(branch = this.root) {
+    if (!branch) return 0;
 
+    let leftStep = 1;
+    let rightStep = 1;
+
+    if (branch.left) {
+      leftStep = 1 + this.maxDepth(branch.left);
+    }
+
+    if (branch.right) {
+      rightStep = 1 + this.maxDepth(branch.right);
+    }
+
+    if (!leftStep || leftStep < rightStep) return rightStep;
+    return leftStep;
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
-  maxSum() {
+  maxSum(branch = this.root) {
+    let result = 0;
 
+    function maxSumHelper(node) {
+      if (node === null) return 0;
+      const leftSum = maxSumHelper(node.left);
+      const rightSum = maxSumHelper(node.right);
+      result = Math.max(result, node.val + leftSum + rightSum);
+      return Math.max(0, leftSum + node.val, rightSum + node.val);
+    }
+
+    maxSumHelper(this.root);
+    return result;
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound) {
+    if (!this.root) return null;
 
+    // let's use BFS for this!
+    let queue = [this.root];
+    let closest = null;
+
+    while (queue.length) {
+      let currentNode = queue.shift();
+      let currentVal = currentNode.val;
+      let higherThanLowerBound = currentVal > lowerBound;
+      let shouldReassignClosest = currentVal < closest || closest === null;
+
+      if (higherThanLowerBound && shouldReassignClosest) {
+        closest = currentVal;
+      }
+
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+
+    return closest;
   }
 
   /** Further study!
--- tree.js
+++ tree.js
@@ -12,23 +12,56 @@ class Tree {
     this.root = root;
   }
 
+  //Helper recursive method
+
   /** sumValues(): add up all of the values in the tree. */
 
-  sumValues() {
+  sumValues(branch = this.root) {
+    if(!branch) return 0;
+
+    let sum = 0;
+    sum += branch.val;
+
+    branch.children.forEach(child => {
+      sum += this.sumValues(child);
+    });
+
+    return sum;
     
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
-  countEvens() {
+  countEvens(branch = this.root) {
+    if(!branch) return 0;
 
+    let count = 0;
+    if (branch.val % 2 == 0) {
+      count ++;
+    }
+
+    branch.children.forEach(child => {
+      count += this.countEvens(child);
+    });
+
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
-  numGreater(lowerBound) {
+  numGreater(lowerBound, branch = this.root) {
+    if(!branch) return 0;
 
+    let count = 0;
+
+    if (branch.val > lowerBound) count++;
+
+    branch.children.forEach(child => {
+      count += this.numGreater(lowerBound,child);
+    });
+
+    return count;
   }
 }
 
